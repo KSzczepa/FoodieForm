43c2305fe936dee3c5b755b8c8d9722c06c64828
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-15 18:21:34 +0200

    Configuration files

---
+++ src/types/FormValues.tsx
@@ -0,0 +1,10 @@
+
+export type FormValues =  {
+    dishName: string;
+    preparationTime: number;
+    dishType: string;
+    noSlices?: number;
+    diameter?: number;
+    spiciness?: number;
+    slicesOfBread?: number;
+}
