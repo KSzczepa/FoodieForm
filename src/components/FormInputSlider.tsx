8ef07438f8556f0e2befdf7fb7c905776291bfdc
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-15 18:22:39 +0200

    Slider component

---
+++ src/components/FormInputSlider.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Slider } from "@mui/material";
+import { Control, Controller, UseFormSetValue } from "react-hook-form";
+
+interface sliderProps {
+    name: string;
+    control: Control<any, any>;
+    setValue: UseFormSetValue<any>;
+    label: string;
+    min: number;
+    max: number;
+    step: number;
+    defaultValue: number;
+}
+
+export const FormInputSlider:
+    React.FC<sliderProps> =
+    ({ name, control, setValue, label, min=0, max=100, step=1, defaultValue=0 }) => {
+        const [sliderValue, setSliderValue] = React.useState(0);
+
+        useEffect(() => {
+            if (sliderValue) setValue(name, sliderValue);
+        }, [sliderValue]);
+
+        const handleChange = (event: any, newValue: number | number[]) => {
+            setSliderValue(newValue as number);
+        };
+
+        return <Controller
+            name={name}
+            control={control}
+            render={({ field, fieldState, formState }) => (
+                <Slider
+                    value={sliderValue}
+                    onChange={handleChange}
+                    aria-label={label}
+                    min={min}
+                    max={max}
+                    step={step}
+                    defaultValue={defaultValue}
+                    marks
+                    valueLabelDisplay="auto"
+                />
+            )}
+        />
+    };
