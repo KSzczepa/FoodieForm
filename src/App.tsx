daaee686040d7b88e12b79e6609af17f73a0d70d
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-15 21:37:19 +0200

    App: error notifications included
    Toast notification included after post request

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { Fragment } from 'react';
 import './App.css';
 import MealForm from './components/MealForm';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <MealForm/>
-    </div>
+    <Fragment>
+      <MealForm />
+      <ToastContainer />
+    </Fragment>
   );
 }
 
