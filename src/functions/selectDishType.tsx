0fe012e339c8509676b42d29d56891d3dca7fe4b
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-15 18:23:26 +0200

    Conditional form

---
+++ src/functions/selectDishType.tsx
@@ -0,0 +1,132 @@
+import { FormControl, FormHelperText, InputLabel, SelectChangeEvent, TextField } from "@mui/material";
+import { Control, Controller, FieldErrors, FormState, UseFormRegister, UseFormSetValue, useForm } from "react-hook-form";
+import { FormInputSlider } from "../components/FormInputSlider";
+
+import styles from '../components/MealForm.module.css';
+import { useState } from "react";
+
+
+
+export function selectedDishType(
+    register: UseFormRegister<any>, control: Control<any, any>,
+    setValue: UseFormSetValue<any>, errors: FieldErrors<any>, dishType: string) {
+
+
+    let result: JSX.Element = <div></div>;
+
+    const no_of_slices =
+        <div style={{ padding: '15px' }}>
+            <FormControl fullWidth>
+                <TextField
+                    label="Number of slices"
+                    error={Boolean(errors.noSlices)}
+                    helperText={errors.noSlices ? String(errors.noSlices?.message) : ''}
+                    type="number"
+                    variant="outlined"
+                    InputProps={{ inputProps: { min: 1, max: 100 } }}
+                    {...register("noSlices", {
+                        required: dishType === "pizza" ? "This field is required" : false,
+                        min: {
+                            value: 1,
+                            message: "Minimum value is 1"
+                        },
+                    })
+                    }
+                />
+                {/* {errors.noSlices && <FormHelperText style={{ color: "#d32f2f" }}>
+                    {String(errors.noSlices.message)}</FormHelperText>} */}
+            </FormControl >
+        </div>
+
+    const diameter =
+        <div style={{ padding: '15px' }}>
+            <FormControl fullWidth>
+                <TextField
+                    error={Boolean(errors.diameter)}
+                    helperText={errors.diameter ? String(errors.diameter?.message) : ''}
+                    type="number"
+                    label="Diameter in cm"
+                    variant="outlined"
+                    InputProps={{ inputProps: { min: 5, max: 100, step: 0.1 } }}
+                    {...register("diameter", {
+                        required: dishType === "pizza" ? "This field is required" : false,
+                        min: {
+                            value: 5,
+                            message: "Minimum value is 5"
+                        },
+                        max: {
+                            value: 100,
+                            message: "Max value is 100"
+                        }
+                    })} />
+                {/* {errors.diameter && <FormHelperText style={{ color: "#d32f2f" }}>{String(errors.diameter.message)}</FormHelperText>} */}
+            </FormControl >
+        </div>
+
+    const spiciness_scale =
+        <div style={{ padding: '15px' }}>
+            <FormControl fullWidth>
+                <InputLabel className={styles.inputLabel}>Spiciness scale</InputLabel>
+                <FormInputSlider name={"spiciness"}
+                    control={control}
+                    setValue={setValue}
+                    label={"Spiciness scale"}
+                    min={1}
+                    max={10}
+                    step={1}
+                    defaultValue={5}
+                />
+                {errors.spiciness && <p>This information is required.</p>}
+            </FormControl >
+        </div>
+
+    const slices_of_bread =
+        <div style={{ padding: '15px' }}>
+            <FormControl fullWidth>
+                <TextField
+                    error={Boolean(errors.slicesOfBread)}
+                    helperText={errors.slicesOfBread ? String(errors.slicesOfBread?.message) : ''}
+                    type="number"
+                    label="Number of slices of bread"
+                    variant="outlined"
+                    {...register("slicesOfBread", {
+                        required: dishType === "sandwich" ? "This field is required" : false,
+                        min: {
+                            value: 1,
+                            message: "Minimum value is 1"
+                        },
+                    })} />
+            </FormControl >
+        </div>
+
+    switch (dishType) {
+        case "pizza":
+            {
+                result = <div>
+                    {no_of_slices}
+                    {diameter}
+                </div>;
+                break;
+            }
+        case "soup":
+            {
+                result = spiciness_scale;
+                break;
+            }
+        case "sandwich":
+            {
+                result = slices_of_bread;
+                break;
+            }
+        default:
+            result = <div />;
+
+    }
+
+    return result;
+}
+
+
+export { }
+
+
