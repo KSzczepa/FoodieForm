328020e5ca5e829bf7629d8fe531161559229845
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-16 00:47:17 +0200

    Changed file name
    filter function moved to modifyFormData.tsx

--- src/functions/validation.tsx
+++
@@ -1,6 +0,0 @@
-import { FormValues } from '../types/FormValues'
-
-export function FilterFormData(data: FormValues) {
-    return Object.fromEntries(Object.entries(data).filter(([key, value]) => value !== undefined)) as FormValues;
-}
-
