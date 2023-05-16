ba891cac48e25ffce4f626b002dafbadac0f8686
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-16 19:06:16 +0200

    Update types and global variables
    created keys for conditional display fields and pairs with dishtype

--- src/types/FormValues.tsx
+++ src/types/FormValues.tsx
@@ -8,3 +8,25 @@ export type FormValues =  {
     spiciness_scale?: number;
     slices_of_bread?: number;
 }
+
+export type DishTypePairs = {
+    name: string;
+    keys: string | string[]
+}
+
+export const dishTypePairs: DishTypePairs[] = [
+    { 
+        name: "pizza",
+        keys: ["no_of_slices", "diameter"],
+    },
+    { 
+        name: "soup",
+        keys: ["spiciness_scale"],
+    },
+    { 
+        name: "sandwich",
+        keys: ["slices_of_bread"],
+    }
+]
+
+export const allKeysForConditionalField = ["no_of_slices", "diameter", "spiciness_scale", "slices_of_bread"];
