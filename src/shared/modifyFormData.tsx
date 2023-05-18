4b5608e3d96707a3097ebb60965d9e44c7d40775
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-18 23:43:14 +0200

    modifyFormFunctions
    deleted post request function
    changes resetUnselectedField fcn

--- src/shared/modifyFormData.tsx
+++ src/shared/modifyFormData.tsx
@@ -1,33 +1,17 @@
 import { FormValues } from '../assets/types/FormValuesType'
-import { allkeysForConditionalField, pairsOfDishAndKey } from '../assets/mealFormDefaultKeyValues'
 import { UseFormResetField } from 'react-hook-form';
 import { toastHandler } from './toastHandler';
+import { pairsOfDishAndKey } from '../assets/mealFormDefaultKeyValues';
 
 export function filterFormData(data: FormValues) {
     return Object.fromEntries(Object.entries(data).filter(([key, value]) => value !== undefined)) as FormValues;
 }
 
-export async function postFormData(data: FormValues, url: string) {
 
-    toastHandler('PENDING');
-
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-
-    showPostRequestResult(response, data);
-}
-
-
-export async function showPostRequestResult(result: Response, data: FormValues) {
+export async function showPostRequestResultForMealForm(result: Response, data: FormValues) {
     const receivedData = await result.json();
-    console.log(receivedData);    
 
-    if (result.ok) {        
+    if (result.ok) {
         toastHandler('SUCCESS', 'Message sent!');
     }
 
@@ -38,40 +22,15 @@ export async function showPostRequestResult(result: Response, data: FormValues) 
             if (receivedData[key]) {
                 errorMessage = errorMessage + key + ": " + receivedData[key][0] + " ";
             }
-
         }
-        
         toastHandler('ERROR', errorMessage);
     }
-} 
+}
 
 
-export function resetUnselectedFields(type: string, resetField: UseFormResetField<FormValues>) {  
-      
-    switch (type) {
-        case "pizza":
-            {                
-                resetField("spiciness_scale");
-                resetField("slices_of_bread");
-                break;
-            }
-        case "soup":
-            {
-                resetField("no_of_slices");
-                resetField("diameter");
-                resetField("slices_of_bread");
-                break;
-            }
-        case "sandwich":
-            {
-                resetField("no_of_slices");
-                resetField("diameter");
-                resetField("spiciness_scale");
-                break;
-            }
-        default:
-            break;
-
-    }
+export function resetUnselectedFields(type: string, resetField: UseFormResetField<FormValues>) {
+    pairsOfDishAndKey.filter((it) => it.dishName != type).forEach(element => {
+        element.keyFields.forEach(field=>resetField(field))
+    });
 }
 
