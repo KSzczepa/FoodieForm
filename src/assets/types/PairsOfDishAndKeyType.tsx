1e1a6389ff346ff9a8232d1dda3ccb0c4aa2ec00
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-18 23:41:26 +0200

    update dishpairs
    keyfields type changes

--- src/assets/types/PairsOfDishAndKeyType.tsx
+++ src/assets/types/PairsOfDishAndKeyType.tsx
@@ -1,4 +1,4 @@
 export type ObjectPairArrayWithDishAndKey = {
     dishName: string;
-    keyFields: string | string[]
+    keyFields: ("name" | "preparation_time" | "type" | "no_of_slices" | "diameter" | "spiciness_scale" | "slices_of_bread")[]
 }
