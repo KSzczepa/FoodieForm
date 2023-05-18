5e8a7aa9b738dec4ed1ed268edb1e51e7ceb1428
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-18 23:40:30 +0200

    Toast Handler update
    validation:  is message present

--- src/shared/toastHandler.tsx
+++ src/shared/toastHandler.tsx
@@ -24,38 +24,19 @@ export function toastHandler(toastType: string, message?: string | string[]) {
             toast.success(messageToDisplay, {
                 position: toast.POSITION.TOP_RIGHT
             });
-
-            // setTimeout(() => {
-            //     toast.update('pending', {
-            //         render: 'Message sent!',
-            //         type: toast.TYPE.SUCCESS,
-            //         autoClose: 5000, 
-            //       });                
-            // }, 250);
-
             break;
         }
         case 'ERROR': {
             const defaultMessage = 'An error occured.';
-            // const messageToDisplay = message ? message : defaultMessage;
-            // let messageToDisplay;
-
-            // if (message && message?.length !== undefined && message?.length > 1) {
-            //     // messageToDisplay = 'An error occured.' + <br />;
-            //     for (let i=0; i<message.length; i++) {
-            //         messageToDisplay = messageToDisplay + message[i] + '\n';
-            //     }
-            // }
-            // else 
-            //     messageToDisplay = defaultMessage;
 
             setTimeout(() => {
                 toast.dismiss('pending');
             }, 250);
             
-            toast.error(<div>{defaultMessage}<br />{message}</div>, {
+            toast.error(<div>{defaultMessage}<br />{message ? message : ''}</div>, {
                 position: toast.POSITION.TOP_RIGHT
             });
+            break;
         }
     }
 }
