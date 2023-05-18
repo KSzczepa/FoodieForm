02668fdf08bab5b529468bf211f10b93753924b4
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-18 23:40:51 +0200

    Create Post Request function

---
+++ src/shared/postRequestHandler.tsx
@@ -0,0 +1,15 @@
+import { FormValues } from "../assets/types/FormValuesType";
+
+export async function postFormData(data: FormValues, url: string) {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    return response;
+
+}
